e7c5955bb2c99885ce1abe92b0ad464234e7530b
Sara Wiltberger <wiltbergersara@yahoo.com>
2016-10-14 13:57:54 -0700

    adding login page first

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -28,7 +28,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     .state('app', {
     url: '/app',
     abstract: true,
-    templateUrl: 'templates/menu.html',
+    templateUrl: 'templates/login.html',
     controller: 'AppCtrl'
   })
 
