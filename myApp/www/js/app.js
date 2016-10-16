7afa12c792e32f3675f73e2e15b266ce0029f37c
Sara Wiltberger <wiltbergersara@yahoo.com>
2016-10-15 20:31:28 -0700

    login and sign up properly with JS in controllers and routes in app.js

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -25,53 +25,31 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-
-    .state('app', {
-    url: '/app',
-    //abstract: true,
-    templateUrl: 'templates/login.html',
-    controller: 'AppCtrl'
-  })
     .state('login', {
     url: '/login',
-    abstract: true,
     templateUrl: 'templates/login.html',
-    controller: 'loginCtr'
+    controller: 'LoginCtrl'
   })
   .state('signUp', {
     url: '/signup',
-    //abstract: true,
     templateUrl: 'templates/signUp.html',
-    controller: 'AppCtrl'
+    controller: 'signUpCtrl'
   })
-  .state('app.search', {
-    url: '/search',
+  .state('parker', {
+    url: '/parker',
+    templateUrl: 'templates/parkerMenu.html',
+    controller: 'parkerMenuCtrl'
+  })
+  .state('parker.search', {
+    url: '/parkerSearch',
     views: {
       'menuContent': {
         templateUrl: 'templates/search.html'
       }
-    }
+    },
+    controller: 'parkerSearchCtrl'
   })
 
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/login');
 });
--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -25,9 +25,9 @@ angular.module('starter.controllers', [])
   };
 
   // Open the login modal
-  $scope.login = function() {
+  /*$scope.login = function() {
     $scope.modal.show();
-  };
+  };*/
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
@@ -39,21 +39,88 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+  
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+
+//LogIn Controller
+.controller('LoginCtrl', function($scope, $ionicPopup, $state) {
+    $scope.data = {};
+     console.log("in login controller");
+    $scope.login = function() {
+
+      var username = ""+ $scope.data.username;
+      var password = ""+ $scope.data.password;
+      var userType = document.querySelector('input[name = "loginType"]:checked');
+      console.log(username );
+      console.log(password );
+      if(userType == null){
+        var div = document.getElementById('invalid');
+          div.innerHTML = 'Please select parker or owner';
+          return;
+      }
+      userType = userType.value;
+      console.log(userType );
+      
+      if(password.length == 0 ||  username.length == 0){
+          //invalid login
+          var div = document.getElementById('invalid');
+          div.innerHTML = 'Please insert all fields';
+          return;
+      }
+      $state.go("parker.search");
+    }
+
+    $scope.signUpClicked = function() {
+      console.log("sign up clicked");
+      $state.go("signUp");
+    }
 })
 
-.controller('loginCtrl', function($scope, $stateParams) {
-  $scope.var1 = 'Sara';
+//SignUp Controller
+.controller('signUpCtrl', function($scope, $ionicPopup, $state) {
+  $scope.data = {};
+  $scope.signUp = function(){
+    var username = ""+ $scope.data.username;
+    var email = "" +  $scope.data.email;
+    var password = ""+ $scope.data.password;
+    var userType = document.querySelector('input[name = "signUpType"]:checked');
+    console.log(username);
+    console.log(email);
+    console.log(password);
+    if(userType == null){
+      var div = document.getElementById('invalid');
+        div.innerHTML = 'Please select parker or owner';
+        return;
+    }
+    userType = userType.value;
+    console.log(userType );
+    
+    if(password.length == 0 ||  username.length == 0 || email.length == 0 ){
+        //invalid login
+        var div = document.getElementById('invalid');
+        div.innerHTML = 'Please insert all fields';
+        return;
+    }
+    var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if(password.length < 10 ||  !regularExpression.test(password)){
+          //invalid login
+          var div = document.getElementById('invalid');
+          div.innerHTML = 'Your password must contain special letter and be longer than 10 characters, please try again';
+          return;
+    }
+  }
+  //check if sign up already used
+  //if it is already used for to owner or user page
+
+})
+
+.controller('parkerMenuCtrl', function($scope, $ionicPopup, $state) {
+})
+
+.controller('parkerSearchCtrl', function($scope, $ionicPopup, $state) {
+})
 
 
-});
+
+
