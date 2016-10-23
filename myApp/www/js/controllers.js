5533eb2ff5465d7c9ebf5fb30a215af9b0949a18
Sara Wiltberger <wiltbergersara@yahoo.com>
2016-10-23 16:54:09 -0700

    added payment options

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -211,7 +211,159 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('paypalCtrl', function($scope, $ionicPopup, $state) {
+
+//getting payment token for owner
+.controller('ownerPayCtrl', function($scope, $ionicPopup, $state, StripeCharge, $http) {
+ // add the following headers for authentication
+  $http.defaults.headers.common['X-Mashape-Key']  = NOODLIO_PAY_API_KEY;
+  $http.defaults.headers.common['Content-Type']   = 'application/x-www-form-urlencoded';
+  $http.defaults.headers.common['Accept']         = 'application/json';
+  
+  $scope.FormData = {
+    number: "",
+    cvc: "",
+    exp_month: "",
+    exp_year: "",
+    test: TEST_MODE, 
+  };
+  
+  $scope.createToken = function() {
+    
+    // init for the DOM
+    $scope.ResponseData = {
+      loading: true
+    };
+    
+    // create a token and validate the credit card details
+    $http.post(NOODLIO_PAY_API_URL + "/tokens/create", $scope.FormData)
+    .success(
+      function(response){
+        
+        // --> success
+        console.log(response)
+        
+        if(response.hasOwnProperty('id')) {
+          var token = response.id; $scope.ResponseData['token'] = token;
+          proceedCharge(token);
+        } else {
+          $scope.ResponseData['token'] = 'You did not input all fields properly, please try again';
+          $scope.ResponseData['loading'] = false;
+        };
+
+      }
+    )
+    .error(
+      function(response){
+        console.log(response)
+        $scope.ResponseData['token'] = 'You did not input all fields properly, please try again';
+        $scope.ResponseData['loading'] = false;
+      }
+    );
+  };
+  
+  // charge the customer with the token
+  function proceedCharge(token) {
+    
+    var param = {
+      source: token,
+      amount: 100,
+      currency: "usd",
+      description: "Your custom description here",
+      stripe_account: STRIPE_ACCOUNT_ID,
+      test: TEST_MODE,
+    };
+    
+    $http.post(NOODLIO_PAY_API_URL + "/charge/token", param)
+    .success(
+      function(response){
+        
+        // --> success
+        console.log(response);
+        $scope.ResponseData['loading'] = false;
+        
+        if(response.hasOwnProperty('id')) {
+          var paymentId = response.id; $scope.ResponseData['paymentId'] = paymentId;
+        } else {
+          $scope.ResponseData['paymentId'] = 'Error, see console';
+        };
+        
+      }
+    )
+    .error(
+      function(response){
+        console.log(response)
+        $scope.ResponseData['paymentId'] = 'Error, see console';
+        $scope.ResponseData['loading'] = false;
+      }
+    );
+  };
+  
+})
+
+
+//where we set up the payment... should be for parker 
+.controller('parkerPayCtrl', function($scope, $ionicPopup, $state, StripeCharge) {
+    $scope.ProductMeta = {
+    title: "Awesome product",
+    description: "Yes it really is",
+    priceUSD: 1,
+  };
+
+  $scope.status = {
+    loading: false,
+    message: "",
+  };
+
+  $scope.charge = function() {
+
+    $scope.status['loading'] = true;
+    $scope.status['message'] = "Retrieving your Stripe Token...";
+
+    // first get the Stripe token
+    StripeCharge.getStripeToken($scope.ProductMeta).then(
+      function(stripeToken){
+        // -->
+        proceedCharge(stripeToken);
+      },
+      function(error){
+        console.log(error)
+
+        $scope.status['loading'] = false;
+        if(error != "ERROR_CANCEL") {
+          $scope.status['message'] = "Oops... something went wrong";
+        } else {
+          $scope.status['message'] = "";
+        }
+      }
+    ); // ./ getStripeToken
+
+    function proceedCharge(stripeToken) {
+
+      $scope.status['message'] = "Processing your payment...";
+
+      // then chare the user through your custom node.js server (server-side)
+      StripeCharge.chargeUser(stripeToken, $scope.ProductMeta).then(
+        function(StripeInvoiceData){
+          
+          if(StripeInvoiceData.hasOwnProperty('id')) {
+            $scope.status['message'] = "Success! Check your Stripe Account";
+          } else {
+            $scope.status['message'] = "Error, check your console";
+          };
+          $scope.status['loading'] = false;
+          console.log(StripeInvoiceData)
+        },
+        function(error){
+          console.log(error);
+
+          $scope.status['loading'] = false;
+          $scope.status['message'] = "Oops... something went wrong";
+        }
+      );
+
+    }; // ./ proceedCharge
+
+  };
 })
 
 //owner controller
@@ -226,8 +378,7 @@ angular.module('starter.controllers', [])
     $state.go("owner.profile");
   }
   $scope.payment = function() {
-      console.log("payment clicked");
-      $state.go("parker.paypal");
+      $state.go("owner.payment");
     }
     //rating
   $scope.ratingsObject = {
@@ -283,7 +434,6 @@ angular.module('starter.controllers', [])
     });
   };
 })
-
 
 .controller('ownerHomeCtrl', function($scope, $ionicPopup, $state) {
  
