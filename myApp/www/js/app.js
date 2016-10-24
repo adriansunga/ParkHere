e24b3f8c41ddaa10429ab7fbe7b265279b97dbb3
colebrossart <cole.brossart@gmail.com>
2016-10-23 20:32:20 -0700

    hey sara

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic-timepicker','ionic-ratings', 'ionic-datepicker', 'starter.controllers'])
-.run(function($ionicPlatform) {
+var example = angular.module('starter', ['ionic', 'ionic-timepicker','ionic-ratings', 'ionic-datepicker', 'starter.controllers'])
+example.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,10 +18,53 @@ angular.module('starter', ['ionic', 'ionic-timepicker','ionic-ratings', 'ionic-d
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    Parse.initialize("parkHere");
+    Parse.serverURL = 'http://138.68.43.212:1337/parse';
+
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+
+
+example.controller("ExampleController", function($scope) {
+ 
+    $scope.savePerson = function(firstname, lastname) {
+        var PeopleObject = Parse.Object.extend("PeopleObject");
+        var person = new PeopleObject();
+        person.set("firstname", firstname);
+        person.set("lastname", lastname);
+        person.save(null, {});
+    };
+
+    $scope.getPeople = function(params) {
+    var PeopleObject = Parse.Object.extend("PeopleObject");
+    var query = new Parse.Query(PeopleObject);
+    if(params !== undefined) {
+        if(params.lastname !== undefined) {
+            query.equalTo("lastname", params.lastname);
+        }
+        if(params.firstname !== undefined) {
+            query.equalTo("firstname", params.lastname);
+        }
+    }
+    query.find({
+        success: function(results) {
+            alert("Successfully retrieved " + results.length + " people!");
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
+                console.log(object.id + ' - ' + object.get("firstname") + " " + object.get("lastname"));
+            }
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+};
+ 
+});
+
+example.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('login', {
