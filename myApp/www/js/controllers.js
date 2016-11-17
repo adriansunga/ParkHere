7cc99846dd9158d833ba6a8a48dcc81e21b37edc
Adrian Sunga <asunga@usc.edu>
2016-11-16 21:34:32 -0800

    Fixed sessions handling when user deleted in database

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -10,13 +10,22 @@ angular.module('starter.controllers', [])
 
     var currUser = Parse.User.current();
     if (currUser != null) {
-        var userType = currUser.get("userType");
-
-        if (userType == 'parker') {
-            $state.go('parker.search');
+        var currObjID = currUser.get("objectId");
+        if(currObjID == null) {
+            console.log("Curr user doesn't exist");
+            currUser.logOut();
         } else {
-            $state.go('owner.home');
+            console.log("Curr user exists!");
+            var userType = currUser.get("userType");
+
+            if (userType == 'parker') {
+                $state.go('parker.search');
+            } else {
+                $state.go('owner.home');
+            }
         }
+
+        
     }
 
     $scope.login = function() {
